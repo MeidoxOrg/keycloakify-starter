ac8c37f6322f63aea1286cfab911a95caad271c6
lieudvq0302 <lieudvq0302@gmail.com>
2025-09-10 22:29:06 +0700

    Test

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,7 +3,7 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
+import Template from "./Template";
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -37,4 +37,7 @@ export default function KcPage(props: { kcContext: KcContext }) {
     );
 }
 
-const classes = {} satisfies { [key in ClassKey]?: string };
+const classes = {
+    kcHtmlClass: "",
+    kcBodyClass: ""
+} satisfies { [key in ClassKey]?: string };
