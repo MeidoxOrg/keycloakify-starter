63cd699d37794ece74a7c2c93aad573158393efd
lieudvq0302 <lieudvq0302@gmail.com>
2025-09-11 23:10:49 +0700

    Update keycloakify

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,6 +4,8 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "./Template";
+import { tss } from "tss-react/mui";
+import backgroundJpgUrl from "./assets/img/background.png";
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
@@ -14,7 +16,7 @@ export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
 
     const { i18n } = useI18n({ kcContext });
-
+    const { classes } = useStyles();
     return (
         <Suspense>
             {(() => {
@@ -37,7 +39,15 @@ export default function KcPage(props: { kcContext: KcContext }) {
     );
 }
 
-const classes = {
-    kcHtmlClass: "",
-    kcBodyClass: ""
-} satisfies { [key in ClassKey]?: string };
+const useStyles = tss.create({
+    kcHtmlClass: {
+        ".root": {
+            colorScheme: "dart"
+        }
+    },
+    kcBodyClass: {
+        background: `url(${backgroundJpgUrl}) no-repeat center center fixed`,
+        backgroundSize: "cover",
+        height: "100%"
+    }
+} satisfies { [key in ClassKey]?: unknown });
